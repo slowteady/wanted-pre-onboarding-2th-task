8e9f3f85739321bc718b3911f016f0c34a1fd44f
slowteady <vbn0213@gmail.com>
2023-08-29 23:32:45 +0900

    feat: 이슈 리스트 데이터 호출 및 후처리 로직 구현

--- src/page/IssuesList.jsx
+++ src/page/IssuesList.jsx
@@ -1,6 +1,43 @@
-import React from "react";
+import React, { useEffect, useMemo } from "react";
+import { getIssuesList } from "../api/issuesApi";
+import { ERROR_MESSAGES } from "../utils/errorMessages";
+
+const QUERY_PARAMS = {
+  state: "open",
+  sort: "comments",
+  direction: "desc",
+  per_page: 10,
+};
 
 function IssuesList() {
+  const params = useMemo(() => {
+    return {
+      params: {
+        ...QUERY_PARAMS,
+      },
+    };
+  }, []);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await getIssuesList(params);
+        if (response.status === 200) {
+        } else {
+          const msg = response.data.message ? response.data.message : ERROR_MESSAGES.REQUEST_FAILED;
+          throw new Error(msg);
+        }
+      } catch (err) {
+        if (err instanceof Error) {
+          alert(`Error: ${err.message}`);
+        } else {
+          alert(ERROR_MESSAGES.REQUEST_FAILED);
+        }
+      }
+    }
+    fetchData();
+  }, [params]);
+
   return <div>IssuesList</div>;
 }
 
