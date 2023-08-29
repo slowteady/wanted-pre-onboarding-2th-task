f0489eee5b80b97412b654a1d6332c42fec78ad1
slowteady <vbn0213@gmail.com>
2023-08-29 20:30:30 +0900

    refactor: 라우터 path에 맞는 컴포넌트 출력하도록 수정

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
+import IssuesList from "../page/IssuesList";
+import IssuesRead from "../page/IssuesRead";
 
 function Router() {
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/list" />} replace />
-      <Route path="/read/:id" element={<Navigate to="/read" />} />
+      <Route path="/list" element={<IssuesList />} />
+      <Route path="/read/:id" element={<IssuesRead />} />
     </Routes>
   );
 }
