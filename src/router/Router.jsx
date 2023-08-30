52e27c7cd94f0cfc07b2058ab8547bc375e7d92e
slowteady <vbn0213@gmail.com>
2023-08-30 11:54:04 +0900

    feat: Isseus 페이지 라우터 연결

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,12 +1,12 @@
-import React from "react";
-import { Navigate, Route, Routes } from "react-router-dom";
-import IssuesList from "../page/IssuesList";
+import React from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import Issues from '../page/issues/Issues';
 
 function Router() {
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/list" />} replace />
-      <Route path="/list" element={<IssuesList />} />
+      <Route path="/list" element={<Issues />} />
     </Routes>
   );
 }
