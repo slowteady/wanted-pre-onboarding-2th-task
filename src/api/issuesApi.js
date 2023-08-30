a8084a0646bbe686af3ea41f71341ab4b96d0322
slowteady <vbn0213@gmail.com>
2023-08-31 00:44:34 +0900

    feat: 이슈 디테일 데이터 요청 함수 추가

--- src/api/issuesApi.js
+++ src/api/issuesApi.js
@@ -3,3 +3,7 @@ import { apiClient } from './apiClient';
 export const getIssuesList = (params) => {
   return apiClient.get('', params);
 };
+
+export const getIssuesDetail = (id) => {
+  return apiClient.get(`/${id}`);
+};
