919efa9488ba7a0d8c869dfec0b1f8215c59d474
slowteady <vbn0213@gmail.com>
2023-08-30 12:54:41 +0900

    feat: "*" path 에러페이지로 중계 하도록 구현

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
+import Error from '../page/error/Error';
 import Issues from '../page/issues/Issues';
 
 function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/list" />} replace />
-      <Route path="/list" element={<Issues />} />
+      <Route path="/" element={<Navigate to="/issues" />} replace />
+      <Route path="/issues" element={<Issues />} />
+      <Route path="*" element={<Error />} />
     </Routes>
   );
 }
