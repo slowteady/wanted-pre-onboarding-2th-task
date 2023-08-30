4407267b507996348ebbad8fc40e52df6ad442d3
slowteady <vbn0213@gmail.com>
2023-08-31 00:45:00 +0900

    refactor: 객체 path 변경

--- src/router/routerPaths.js
+++ src/router/routerPaths.js
@@ -1,7 +1,7 @@
 const routerPaths = {
   home: { path: '/', name: 'Home' },
   issues: { path: '/issues', name: 'Issues' },
-  issuesDetail: { path: '/issues/:id', name: 'IssuesDetail' },
+  issuesDetail: { path: ':id', name: 'IssuesDetail' },
   errorRedirect: { path: '*', name: 'ErrorRedirect' },
   error: { path: '/error', name: 'Error' },
 };
