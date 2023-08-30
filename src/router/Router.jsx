ad4213486a63c7f0abd338113785a755a4b94da2
slowteady <vbn0213@gmail.com>
2023-08-31 00:45:33 +0900

    refactor: 라우터 useRoutes 방식으로 변경

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,19 +1,39 @@
 import React from 'react';
-import { Navigate, Route, Routes } from 'react-router-dom';
+import { Navigate, useRoutes } from 'react-router-dom';
+import Layout from '../components/layout/issues/Layout';
 import Error from '../page/error/Error';
 import Issues from '../page/issues/Issues';
+import IssuesDetail from '../page/issues/IssuesDetail';
 import routerPaths from './routerPaths';
 
 function Router() {
-  return (
-    <Routes>
-      <Route path={routerPaths.home.path} element={<Navigate to={routerPaths.issues.path} />} replace />
-      <Route path={routerPaths.errorRedirect.path} element={<Navigate to={routerPaths.error.path} />} replace />
-      <Route path={routerPaths.issues.path} element={<Issues />} />
-      <Route path={routerPaths.issuesDetail.path} element={<Issues />} />
-      <Route path={routerPaths.error.path} element={<Error />} />
-    </Routes>
-  );
+  const routes = useRoutes([
+    {
+      path: routerPaths.home.path,
+      element: <Navigate to={routerPaths.issues.path} replace />,
+    },
+    {
+      path: routerPaths.issues.path,
+      element: <Layout />,
+      children: [
+        { element: <Issues />, index: true },
+        {
+          path: routerPaths.issuesDetail.path,
+          element: <IssuesDetail />,
+        },
+      ],
+    },
+    {
+      path: routerPaths.errorRedirect.path,
+      element: <Navigate to={routerPaths.error.path} replace />,
+    },
+    {
+      path: routerPaths.error.path,
+      element: <Error />,
+    },
+  ]);
+
+  return routes;
 }
 
 export default Router;
