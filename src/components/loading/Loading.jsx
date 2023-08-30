6107b03ff3f8b8d424fc99fd76c06f3339989fd3
slowteady <vbn0213@gmail.com>
2023-08-30 12:57:10 +0900

    feat: 로딩 컴포넌트 추가

---
+++ src/components/loading/Loading.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import styled, { keyframes } from 'styled-components';
+
+function Loading() {
+  return (
+    <LoadingOverlay>
+      <Spinner />
+    </LoadingOverlay>
+  );
+}
+
+const LoadingOverlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(255, 255, 255, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const spin = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+const Spinner = styled.div`
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  border: 5px solid #ccc;
+  border-top-color: #333;
+  animation: ${spin} 1s linear infinite;
+`;
+
+export default Loading;
