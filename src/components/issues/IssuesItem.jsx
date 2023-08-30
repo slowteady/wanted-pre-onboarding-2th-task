04a01e0d2a872646f7942ecb6c615ccd6f09d726
slowteady <vbn0213@gmail.com>
2023-08-30 11:54:49 +0900

    feat: IssuesItem 컴포넌트 추가

---
+++ src/components/issues/IssuesItem.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const changeDateFormat = (createAt) => {
+  const date = new Date(createAt);
+  return `${date.getFullYear()}년 ${date.getMonth() + 1}월 ${date.getDate()}일`;
+};
+
+function IssuesItem({ issues }) {
+  const {
+    number,
+    title,
+    user: { login },
+    created_at,
+    comments,
+  } = issues;
+
+  return (
+    <Li key={number}>
+      <IssuesDiv>
+        <IssuesNumTitleDiv>
+          <IssueNumSpan>{number}</IssueNumSpan>
+          <IssueTitleSpan title={title}>{title}</IssueTitleSpan>
+        </IssuesNumTitleDiv>
+        <IssueDateWriterDiv>
+          <span>
+            작성자: {login}, 작성일: {changeDateFormat(created_at)}
+          </span>
+        </IssueDateWriterDiv>
+      </IssuesDiv>
+      <IssueCommentSpan>코멘트: {comments}</IssueCommentSpan>
+    </Li>
+  );
+}
+
+const Li = styled.li`
+  margin: 10px 0;
+  display: flex;
+  justify-content: space-between;
+  height: 60px;
+  border-bottom: 1px solid #dcd2d2;
+  align-items: center;
+`;
+const IssuesDiv = styled.div`
+  margin: 5px 0 10px;
+`;
+const IssuesNumTitleDiv = styled.div`
+  max-width: 550px;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  font-size: 17px;
+`;
+const IssueNumSpan = styled.span`
+  font-weight: bold;
+  margin: 10px;
+`;
+const IssueTitleSpan = styled.span`
+  font-weight: bold;
+  cursor: pointer;
+`;
+const IssueDateWriterDiv = styled.div`
+  margin: 4px 0 0 10px;
+  color: gray;
+`;
+const IssueCommentSpan = styled.span`
+  margin: 0 10px 5px;
+`;
+
+export default IssuesItem;
